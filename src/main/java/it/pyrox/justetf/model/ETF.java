cdf5747da0be4b4aedaab0628fc63bb4ac662256
Pyr0x1 <Pyr0x1@users.noreply.github.com>
2024-01-31 14:16:34 +0100

    Set surefire plugin version and added new fields to ETF model class

--- src/main/java/it/pyrox/justetf/model/ETF.java
+++ src/main/java/it/pyrox/justetf/model/ETF.java
@@ -5,6 +5,7 @@ public class ETF {
 	private String fiveYearVolatilityCUR;
 	private String fiveYearReturnPerRiskCUR;
 	private String yearVolatilityCUR;
+	private String sustainable;
 	private String distributionPolicy;
 	private String fundCurrency;
 	private String yearDividendYield;
@@ -26,11 +27,15 @@ public class ETF {
 	private String ticker;
 	private String yearReturnCUR;
 	private String fundSize;
+	private String yearMaxDrawdownCUR;
 	private String threeYearReturnPerRiskCUR;
+	private String threeYearMaxDrawdownCUR;
 	private String domicileCountry;
 	private String threeYearReturnCUR;
 	private String valorNumber;
+	private String fiveYearMaxDrawdownCUR;
 	private String currentDividendYield;
+	private String maxDrawdownCUR;
 	private String name;
 	private String yearReturn3CUR;
 	private String fiveYearReturnCUR;
@@ -40,311 +45,360 @@ public class ETF {
 	private String savingsPlanReady;
 	private String isin;
 	private String yearReturn1CUR;
+	private String numberOfHoldings;
 	// This field is populated by calling the relative api
 	private Quote quote;
-	
+
 	public String getFiveYearVolatilityCUR() {
 		return fiveYearVolatilityCUR;
 	}
-	
+
 	public void setFiveYearVolatilityCUR(String fiveYearVolatilityCUR) {
 		this.fiveYearVolatilityCUR = fiveYearVolatilityCUR;
 	}
-	
+
 	public String getFiveYearReturnPerRiskCUR() {
 		return fiveYearReturnPerRiskCUR;
 	}
-	
+
 	public void setFiveYearReturnPerRiskCUR(String fiveYearReturnPerRiskCUR) {
 		this.fiveYearReturnPerRiskCUR = fiveYearReturnPerRiskCUR;
 	}
-	
+
 	public String getYearVolatilityCUR() {
 		return yearVolatilityCUR;
 	}
-	
+
 	public void setYearVolatilityCUR(String yearVolatilityCUR) {
 		this.yearVolatilityCUR = yearVolatilityCUR;
 	}
-	
+
+	public String getSustainable() {
+		return sustainable;
+	}
+
+	public void setSustainable(String sustainable) {
+		this.sustainable = sustainable;
+	}
+
 	public String getDistributionPolicy() {
 		return distributionPolicy;
 	}
-	
+
 	public void setDistributionPolicy(String distributionPolicy) {
 		this.distributionPolicy = distributionPolicy;
 	}
-	
+
 	public String getFundCurrency() {
 		return fundCurrency;
 	}
-	
+
 	public void setFundCurrency(String fundCurrency) {
 		this.fundCurrency = fundCurrency;
 	}
-	
+
 	public String getYearDividendYield() {
 		return yearDividendYield;
 	}
-	
+
 	public void setYearDividendYield(String yearDividendYield) {
 		this.yearDividendYield = yearDividendYield;
 	}
-	
+
 	public String getThreeMonthReturnCUR() {
 		return threeMonthReturnCUR;
 	}
-	
+
 	public void setThreeMonthReturnCUR(String threeMonthReturnCUR) {
 		this.threeMonthReturnCUR = threeMonthReturnCUR;
 	}
-	
+
 	public String getMonthReturnCUR() {
 		return monthReturnCUR;
 	}
-	
+
 	public void setMonthReturnCUR(String monthReturnCUR) {
 		this.monthReturnCUR = monthReturnCUR;
 	}
-	
+
 	public String getSixMonthReturnCUR() {
 		return sixMonthReturnCUR;
 	}
-	
+
 	public void setSixMonthReturnCUR(String sixMonthReturnCUR) {
 		this.sixMonthReturnCUR = sixMonthReturnCUR;
 	}
-	
+
 	public String getInceptionDate() {
 		return inceptionDate;
 	}
-	
+
 	public void setInceptionDate(String inceptionDate) {
 		this.inceptionDate = inceptionDate;
 	}
-	
+
 	public String getThreeYearVolatilityCUR() {
 		return threeYearVolatilityCUR;
 	}
-	
+
 	public void setThreeYearVolatilityCUR(String threeYearVolatilityCUR) {
 		this.threeYearVolatilityCUR = threeYearVolatilityCUR;
 	}
-	
+
 	public String getYearReturnPerRiskCUR() {
 		return yearReturnPerRiskCUR;
 	}
-	
+
 	public void setYearReturnPerRiskCUR(String yearReturnPerRiskCUR) {
 		this.yearReturnPerRiskCUR = yearReturnPerRiskCUR;
 	}
-	
+
 	public String getYearReturn2CUR() {
 		return yearReturn2CUR;
 	}
-	
+
 	public void setYearReturn2CUR(String yearReturn2CUR) {
 		this.yearReturn2CUR = yearReturn2CUR;
 	}
-	
+
 	public String getYearReturn4CUR() {
 		return yearReturn4CUR;
 	}
-	
+
 	public void setYearReturn4CUR(String yearReturn4CUR) {
 		this.yearReturn4CUR = yearReturn4CUR;
 	}
-	
+
 	public String getGroupValue() {
 		return groupValue;
 	}
-	
+
 	public void setGroupValue(String groupValue) {
 		this.groupValue = groupValue;
 	}
-	
+
 	public String getGroupParam() {
 		return groupParam;
 	}
-	
+
 	public void setGroupParam(String groupParam) {
 		this.groupParam = groupParam;
 	}
-	
+
 	public String getWkn() {
 		return wkn;
 	}
-	
+
 	public void setWkn(String wkn) {
 		this.wkn = wkn;
 	}
-	
+
 	public Boolean getSelected() {
 		return selected;
 	}
-	
+
 	public void setSelected(Boolean selected) {
 		this.selected = selected;
 	}
-	
+
 	public String getTer() {
 		return ter;
 	}
-	
+
 	public void setTer(String ter) {
 		this.ter = ter;
 	}
-	
+
 	public String getReplicationMethod() {
 		return replicationMethod;
 	}
-	
+
 	public void setReplicationMethod(String replicationMethod) {
 		this.replicationMethod = replicationMethod;
 	}
-	
+
 	public String getHasSecuritiesLending() {
 		return hasSecuritiesLending;
 	}
-	
+
 	public void setHasSecuritiesLending(String hasSecuritiesLending) {
 		this.hasSecuritiesLending = hasSecuritiesLending;
 	}
-	
+
 	public String getTicker() {
 		return ticker;
 	}
-	
+
 	public void setTicker(String ticker) {
 		this.ticker = ticker;
 	}
-	
+
 	public String getYearReturnCUR() {
 		return yearReturnCUR;
 	}
-	
+
 	public void setYearReturnCUR(String yearReturnCUR) {
 		this.yearReturnCUR = yearReturnCUR;
 	}
-	
+
 	public String getFundSize() {
 		return fundSize;
 	}
-	
+
 	public void setFundSize(String fundSize) {
 		this.fundSize = fundSize;
 	}
-	
+
+	public String getYearMaxDrawdownCUR() {
+		return yearMaxDrawdownCUR;
+	}
+
+	public void setYearMaxDrawdownCUR(String yearMaxDrawdownCUR) {
+		this.yearMaxDrawdownCUR = yearMaxDrawdownCUR;
+	}
+
 	public String getThreeYearReturnPerRiskCUR() {
 		return threeYearReturnPerRiskCUR;
 	}
-	
+
 	public void setThreeYearReturnPerRiskCUR(String threeYearReturnPerRiskCUR) {
 		this.threeYearReturnPerRiskCUR = threeYearReturnPerRiskCUR;
 	}
-	
+
+	public String getThreeYearMaxDrawdownCUR() {
+		return threeYearMaxDrawdownCUR;
+	}
+
+	public void setThreeYearMaxDrawdownCUR(String threeYearMaxDrawdownCUR) {
+		this.threeYearMaxDrawdownCUR = threeYearMaxDrawdownCUR;
+	}
+
 	public String getDomicileCountry() {
 		return domicileCountry;
 	}
-	
+
 	public void setDomicileCountry(String domicileCountry) {
 		this.domicileCountry = domicileCountry;
 	}
-	
+
 	public String getThreeYearReturnCUR() {
 		return threeYearReturnCUR;
 	}
-	
+
 	public void setThreeYearReturnCUR(String threeYearReturnCUR) {
 		this.threeYearReturnCUR = threeYearReturnCUR;
 	}
-	
+
 	public String getValorNumber() {
 		return valorNumber;
 	}
-	
+
 	public void setValorNumber(String valorNumber) {
 		this.valorNumber = valorNumber;
 	}
-	
+
+	public String getFiveYearMaxDrawdownCUR() {
+		return fiveYearMaxDrawdownCUR;
+	}
+
+	public void setFiveYearMaxDrawdownCUR(String fiveYearMaxDrawdownCUR) {
+		this.fiveYearMaxDrawdownCUR = fiveYearMaxDrawdownCUR;
+	}
+
 	public String getCurrentDividendYield() {
 		return currentDividendYield;
 	}
-	
+
 	public void setCurrentDividendYield(String currentDividendYield) {
 		this.currentDividendYield = currentDividendYield;
 	}
-	
+
+	public String getMaxDrawdownCUR() {
+		return maxDrawdownCUR;
+	}
+
+	public void setMaxDrawdownCUR(String maxDrawdownCUR) {
+		this.maxDrawdownCUR = maxDrawdownCUR;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getYearReturn3CUR() {
 		return yearReturn3CUR;
 	}
-	
+
 	public void setYearReturn3CUR(String yearReturn3CUR) {
 		this.yearReturn3CUR = yearReturn3CUR;
 	}
-	
+
 	public String getFiveYearReturnCUR() {
 		return fiveYearReturnCUR;
 	}
-	
+
 	public void setFiveYearReturnCUR(String fiveYearReturnCUR) {
 		this.fiveYearReturnCUR = fiveYearReturnCUR;
 	}
-	
+
 	public String getYtdReturnCUR() {
 		return ytdReturnCUR;
 	}
-	
+
 	public void setYtdReturnCUR(String ytdReturnCUR) {
 		this.ytdReturnCUR = ytdReturnCUR;
 	}
-	
+
 	public String getWeekReturnCUR() {
 		return weekReturnCUR;
 	}
-	
+
 	public void setWeekReturnCUR(String weekReturnCUR) {
 		this.weekReturnCUR = weekReturnCUR;
 	}
-	
+
 	public String getClazz() {
 		return clazz;
 	}
-	
+
 	public void setClazz(String clazz) {
 		this.clazz = clazz;
 	}
-	
+
 	public String getSavingsPlanReady() {
 		return savingsPlanReady;
 	}
-	
+
 	public void setSavingsPlanReady(String savingsPlanReady) {
 		this.savingsPlanReady = savingsPlanReady;
 	}
-	
+
 	public String getIsin() {
 		return isin;
 	}
-	
+
 	public void setIsin(String isin) {
 		this.isin = isin;
 	}
-	
+
 	public String getYearReturn1CUR() {
 		return yearReturn1CUR;
 	}
-	
+
 	public void setYearReturn1CUR(String yearReturn1CUR) {
 		this.yearReturn1CUR = yearReturn1CUR;
+	}
+
+	public String getNumberOfHoldings() {
+		return numberOfHoldings;
+	}
+
+	public void setNumberOfHoldings(String numberOfHoldings) {
+		this.numberOfHoldings = numberOfHoldings;
 	}
 
 	public Quote getQuote() {
@@ -353,5 +407,5 @@ public class ETF {
 
 	public void setQuote(Quote quote) {
 		this.quote = quote;
-	}	
+	}
 }
